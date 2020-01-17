154a76ec52b79af673aae8d018ea01d48dfd4661
nikinov <novellenicholas@gmail.com>
2020-01-17 22:26:33 +0100

    SmallUI changes

--- DeadPixel/Assets/Scripts/DayNightCycle.cs
+++ DeadPixel/Assets/Scripts/DayNightCycle.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Experimental.Rendering.LWRP;
 [RequireComponent(typeof(Light2D))]
 public class DayNightCycle : MonoBehaviour
 {
+    //nikinov
+    public Image BringUpWeapons;
+    public Image SettingsButton;
+
+    //others
     private Light2D _light;
     [SerializeField] private float chengScale;
     [SerializeField] private bool invert;
@@ -12,23 +18,29 @@ public class DayNightCycle : MonoBehaviour
     private bool isDay;
     private float chengIntensityTo;
 
-    private void Awake() {
+    private void Awake()
+    {
         _light = GetComponent<Light2D>();
         chengIntensityTo = 0;
         isDay = true;
     }
 
-    private void Update() {
+    private void Update()
+    {
         _light.intensity = Mathf.Lerp(_light.intensity,chengIntensityTo,Time.deltaTime * chengScale);
 
-        if(_light.intensity < 0.01f && isDay){
+        if(_light.intensity < 0.01f && isDay)
+        {
             isDay = false;
             chengIntensityTo = 1;
         }
-        if(_light.intensity > 0.98f && !isDay){
+        if(_light.intensity > 0.98f && !isDay)
+        {
             isDay = true;
             chengIntensityTo = 0;
         }
+        //more nikinov
+        SettingsButton.color.a.Equals(_light.intensity * 255);
     }
 
 }
