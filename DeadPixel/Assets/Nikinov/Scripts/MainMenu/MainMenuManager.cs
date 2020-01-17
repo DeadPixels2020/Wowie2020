fb921e7bd12c15750303c7f2ec358e684789e408
nikinov <novellenicholas@gmail.com>
2020-01-17 19:09:36 +0100

    MainMenu + other stuf from tower defence zombie

---
+++ DeadPixel/Assets/Nikinov/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    //anime
+    public Animator animator;
+
+    //functions
+    private void Start()
+    {
+
+    }
+    public void GoToPlayScene()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+    public void GoToSettings()
+    {
+        animator.SetInteger("Panels", 1);
+    }
+    public void GoToCreators()
+    {
+        animator.SetInteger("Panels", 2);
+    }
+    public void CloseSettings()
+    {
+        animator.SetInteger("Panels", 3);
+    }
+    public void CloseCreators()
+    {
+        animator.SetInteger("Panels", 4);
+    }
+}
