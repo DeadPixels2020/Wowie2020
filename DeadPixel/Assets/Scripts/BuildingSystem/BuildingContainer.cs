98f8afe0f9c2e224ae647378f53f26a6f5ecf594
Sorpirit <danvuk11@gmail.com>
2020-01-17 17:22:05 +0200

    Setup Building System. Pocket System.

---
+++ DeadPixel/Assets/Scripts/BuildingSystem/BuildingContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingContainer : MonoBehaviour
+{
+    private Building buildingModel;
+
+    public Building BuildingModel{ set => buildingModel = value; get => buildingModel;}
+
+    private GameObject prototype;
+    private GameObject finalBuild;
+
+    public void InstansiatePrototype(){
+        if(finalBuild !=null) return;
+
+        prototype = Instantiate(buildingModel.PrototypeBuilding,transform.position,transform.rotation);
+        prototype.transform.parent = transform;
+
+    }
+
+    public void DestroyPrototype(){
+        if(finalBuild !=null || prototype == null) return;
+
+        Destroy(prototype);
+    }
+
+    public void FinalBuild(){
+        DestroyPrototype();
+        finalBuild = Instantiate(buildingModel.FinishBuilding,transform.position,transform.rotation);
+        finalBuild.transform.parent = transform;
+    }
+}
---
+++ DeadPixel/Assets/Scripts/PocketSystem/PlayerPocket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPocket : MonoBehaviour,IPaymentController,IEarningsController
+{
+
+    public static PlayerPocket Pocket;
+
+    private int mineral;
+    private int metal;
+    private int darkEnegry;
+
+    private void Awake() {
+        if(Pocket == null){
+            Pocket = this;
+        }else{
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    public bool AbleToPay(Costs costs)
+    {
+        return mineral >= costs.Minerals && metal >= costs.Metals && darkEnegry >= costs.DarkEnergy;
+    }
+
+    public void Pay(Costs costs)
+    {
+        mineral -= costs.Minerals;
+        metal -= costs.Metals;
+        darkEnegry -= costs.DarkEnergy;
+    }
+
+    public bool TryToPay(Costs costs)
+    {
+        if(AbleToPay(costs)){
+            Pay(costs);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void AddToPocket(Costs earning)
+    {
+        mineral += earning.Minerals;
+        metal += earning.Metals;
+        darkEnegry += earning.DarkEnergy;
+    }
+}
