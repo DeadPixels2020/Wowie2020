231d6ae39345158141c099f7bc23ad6345d1c458
nikinov <novellenicholas@gmail.com>
2020-01-17 18:40:30 +0100

    MainMenu

--- DeadPixel/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ DeadPixel/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -5,8 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    //anime
+    public Animator animator;
+
+    //functions
+    private void Start()
+    {
+
+    }
     public void GoToPlayScene()
     {
         SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+    public void GoToSettings()
+    {
+        animator.SetInteger("Panels", 1);
+    }
+    public void GoToCreators()
+    {
+        animator.SetInteger("Panels", 2);
+    }
+    public void CloseSettings()
+    {
+        animator.SetInteger("Panels", 3);
+    }
+    public void CloseCreators()
+    {
+        animator.SetInteger("Panels", 4);
     }
 }
