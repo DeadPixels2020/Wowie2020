4455280724a548fa940bc9f084294f385f9c4566
ClitchLich <danvuk11@gmail.com>
2020-01-18 23:42:27 +0200

    Adding Prefabs. Cheng day and night cycle logic. Improve Costes.

--- DeadPixel/Assets/Scripts/DayNightCycle.cs
+++ DeadPixel/Assets/Scripts/DayNightCycle.cs
@@ -10,31 +10,50 @@ public class DayNightCycle : MonoBehaviour
     [SerializeField] private float chengScale;
     [SerializeField] private bool invert;
 
+    [SerializeField] private bool SunriseIfNoEnemiesLeft;
+    [SerializeField] private EnemySpawner spawner;
+
     private bool isDay;
     private float chengIntensityTo;
     
     private IDayNightSeter dayNightSeter;
 
+    public bool IsDay { get => isDay;}
+
     private void Awake() {
         _light = GetComponent<Light2D>();
         chengIntensityTo = 0;
-        isDay = true;
         dayNightSeter = GameEvents.instance;
     }
 
     private void Update() {
         _light.intensity = Mathf.Lerp(_light.intensity,chengIntensityTo,Time.deltaTime * chengScale);
+        
+        if(_light.intensity < 0.001f){
 
-        if(_light.intensity < 0.01f && isDay){
-            isDay = false;
-            chengIntensityTo = 1;
-            if(dayNightSeter != null) dayNightSeter.onGettingDark();
+            if(SunriseIfNoEnemiesLeft){
+                if(spawner.AreAllEnemiesDead()) StartSunrise();
+            }else{
+                StartSunrise();
+            }
+
         }
-        if(_light.intensity > 0.98f && !isDay){
-            isDay = true;
-            chengIntensityTo = 0;
-            if(dayNightSeter != null) dayNightSeter.onGettingBrighter();
+
+        if(_light.intensity > 0.998f){
+            StartSunset();
         }
+
+        isDay = _light.intensity > .5f;
+    }
+
+    private void StartSunrise(){
+        chengIntensityTo = 1;
+        if(dayNightSeter != null) dayNightSeter.onGettingBrighter();
+    }
+
+    private void StartSunset(){
+        chengIntensityTo = 0;
+        if(dayNightSeter != null) dayNightSeter.onGettingDark();
     }
 
 }
