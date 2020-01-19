98041b8a36569846ea8e67391f58146eb6727a8b
ClitchLich <danvuk11@gmail.com>
2020-01-20 01:09:02 +0200

    Turret Update.

---
+++ DeadPixel/Assets/Scripts/UI/PlayerPocketUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerPocketUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text coins;
+
+    private void Start() {
+        PlayerPocket.Pocket.OnMatirialAmountChenged += UpdateCoins;
+        UpdateCoins(PlayerPocket.Pocket.Matirial);
+    }
+
+    private void UpdateCoins(int val){
+        coins.text = val + "";
+    }
+
+    private void OnDestroy() {
+        PlayerPocket.Pocket.OnMatirialAmountChenged += UpdateCoins;
+    }
+}
